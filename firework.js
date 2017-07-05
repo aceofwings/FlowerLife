6d4f09fe7d42f48681d4f82cf8a439dac8cfc41d
DANIEL HARRINGTON <dan303h@gmail.com>
2017-07-05 18:01:52 -0400

    Simply Block stop animation

--- firework.js
+++ firework.js
@@ -3,10 +3,15 @@ var canvas = document.getElementById("draw");
 var ctx = canvas.getContext("2d");
 var circles = [];
 var fps, startTime, now, next, elasped;
+var nextCircle;
+var anchorCircle;
+var interation; 
+
 
 var settings = {
-    maxCircles : 100,
-    frameRate: 60
+    maxCircles : 6,
+    frameRate: 60,
+    gencircle: 1000
 }
 
 //fetch the browsers animation frame function. 
@@ -33,23 +38,50 @@ function centerofCanvas(){
 }
 
 function fireworkTick(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-
-
     
+    if (circles.length > settings.maxCircles){
+	return ;
+    } 
+    
+    ctx.clearRect(0,0,canvas.width,canvas.height);
     requestAnimFrame(fireworkTick);
 
     now = Date.now()
-    
-    elasped = now - then;
 
+   
+    elasped = now - then;
+    lastgen = now - nextCircle;
     if(elasped > fps){
 	then = now - (elasped % fps);
 
 
-	gencircle = now / 3000
+	// lets add a circle
+	if(settings.gencircle < lastgen){
+	    if (circles.length >= section(interation)){
+		interation ++ ;
+	    }
+	    
+	    nextCircle = now;
+	    
+	    circle = newCircle();
+	    
+
+	    theta = 360.0 / section(interation)  ;
+	    loca = (circles.length - 1)  * theta;
+
+	    
+	    addX = circle.radius *  Math.cos(loca);
+	    addY = circle.radius *  Math.sin(loca);
+	    console.log(circles.length)
+	    console.log(theta)
+
+	    circle.position.x = anchorCircle.position.x + addX;
+	    circle.position.y = anchorCircle.position.y + addY;
+	    circles.push(circle);
+	   
+
+	    
 	
-	if(circles.length < gencircle){
 	    
 	    
 	}
@@ -68,13 +100,20 @@ function setup(){
     then = Date.now();
     center = centerofCanvas()
     startTime = then;
+    nextCircle = then;
     circle = newCircle();
     circle.position.x = center.x;
     circle.position.y = center.y;
+    interation = 1;
     circles.push(circle);
-    
-    
-    
+    anchorCircle = circle;
+}
+
+function section(number){
+    temp1 = Math.pow(number,3);
+    temp2 = Math.pow(number-1,3);
+
+    return temp1 - temp2 
 }
 function  update(){
 
